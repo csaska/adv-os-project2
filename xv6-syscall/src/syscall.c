c1cb9041e0f69a4e3565ab8ae20c0d550323a7c8
Colton Saska <colt.saska@gmail.com>
2021-03-12 17:52:16 -0500

    Implement getreadcount system call

--- xv6-syscall/src/syscall.c
+++ xv6-syscall/src/syscall.c
@@ -103,6 +103,7 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+extern int sys_getreadcount(void);
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -126,7 +127,10 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_getreadcount]   sys_getreadcount,
 };
+
+int readcount = 0;
 
 void
 syscall(void)
@@ -135,6 +139,12 @@ syscall(void)
   struct proc *curproc = myproc();
 
   num = curproc->tf->eax;
+  if (num == SYS_read) {
+    ++readcount;
+  }
+  if (num == SYS_getreadcount) {
+    curproc->reads = readcount;
+  }
   if(num > 0 && num < NELEM(syscalls) && syscalls[num]) {
     curproc->tf->eax = syscalls[num]();
   } else {
